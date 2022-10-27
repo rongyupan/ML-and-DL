0fa856f41c158c0093b579decbc4747fe1c766b7
rypan <rongyupan@126.com>
2022-10-27 20:21:53 +0800

    BCELoss修改

--- pytorch/GAN/example/bceloss.py
+++ pytorch/GAN/example/bceloss.py
@@ -9,20 +9,23 @@ import torch
 input = torch.tensor([[ 1.9072,  1.1079,  1.4906],
         [-0.6584, -0.0512,  0.7608],
         [-0.0614,  0.6583,  0.1095]], requires_grad=True)
-print(input)
+print("输入结果：\n", input)
 print('-'*100)
 
 from torch import nn
+# 把 input 中有正有负的值，映射到 [0,1] 之间
 m = nn.Sigmoid()
-print(m(input))
+print("使用Sigmoid进行映射：\n", m(input))
 print('-'*100)
 
+# 随机给定标签值，作为预测结果；
 target = torch.FloatTensor([[0, 1, 1], [1, 1, 1], [0, 0, 0]])
-print(target)
+print("标签值：\n", target)
 print('-'*100)
 
 import math
 
+# 计算交叉熵
 r11 = 0 * math.log(0.8707) + (1-0) * math.log((1 - 0.8707))
 r12 = 1 * math.log(0.7517) + (1-1) * math.log((1 - 0.7517))
 r13 = 1 * math.log(0.8162) + (1-1) * math.log((1 - 0.8162))
@@ -36,18 +39,19 @@ r32 = 0 * math.log(0.6589) + (1-0) * math.log((1 - 0.6589))
 r33 = 0 * math.log(0.5273) + (1-0) * math.log((1 - 0.5273))
 
 r1 = -(r11 + r12 + r13) / 3
-#0.8447112733378236
+# 0.8447112733378236
 r2 = -(r21 + r22 + r23) / 3
-#0.7260397266631787
+# 0.7260397266631787
 r3 = -(r31 + r32 + r33) / 3
-#0.8292933181294807
+# 0.8292933181294807
 bceloss = (r1 + r2 + r3) / 3
-print(bceloss)
+print("手算bceloss：\n", bceloss)
 print('-'*100)
 
 loss = nn.BCELoss()
-print(loss(m(input), target))
+print("调用模块计算loss结果：\n", loss(m(input), target))
 print('-'*100)
 
+# 自动使用sigmoid函数完成对input的处理
 loss = nn.BCEWithLogitsLoss()
 print(loss(input, target))
